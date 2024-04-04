46436dfac3f773519d11f89148036bfc6de6e604
dnaals <swm8793@naver.com>
2024-04-04 09:47:21 +0900

    a

--- src/api/TestMember.js
+++ src/api/TestMember.js
@@ -2,23 +2,23 @@ import axios from 'axios';
 //서버에 요청
 export const fetch = {
     create:async()=>{
-        const res = await axios('http://localhost:3030/create');
+        const res = await axios('https://web-vue-server-4vrp32alrjy7skg.sel5.cloudtype.app/create');
         return res.data
     },
     get:async ()=>{
-        const res = await axios('http://localhost:3030/vue');
+        const res = await axios('https://web-vue-server-4vrp32alrjy7skg.sel5.cloudtype.app/vue');
         return res.data
     },
     post : async (value)=>{
-        const res = await axios.post('http://localhost:3030/vue/insert',value);
+        const res = await axios.post('https://web-vue-server-4vrp32alrjy7skg.sel5.cloudtype.app/insert',value);
         return res.data;
     },
     put :  async (value)=>{
-        const res = await axios.put('http://localhost:3030/vue/update',value);
+        const res = await axios.put('https://web-vue-server-4vrp32alrjy7skg.sel5.cloudtype.app/update',value);
         return res.data;
     },
     delete :  async (num)=>{
-        const res = await axios.delete(`http://localhost:3030/vue/delete?num=${num}`);
+        const res = await axios.delete(`https://web-vue-server-4vrp32alrjy7skg.sel5.cloudtype.app/vue/delete?num=${num}`);
         return res.data;
     }
 }
